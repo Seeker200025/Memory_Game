939b03c1cb0f401d5a45e3168984a5d8ee1ce473
seeker200025 <barkazaoui04@gmail.com>
2022-02-21 20:31:58 +0100

    add script

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const result = document.querySelector('.result');
 
 var tab = [
     [1,0,0,0,],
-    [0,1,0,0,],
-    [0,0,1,0,],
-    [0,1,0,0,]
+    [0,2,0,0,],
+    [0,0,3,0,],
+    [0,4,0,0,]
 ];
-
 
 
 const tablesView = async () =>{
@@ -16,24 +15,63 @@ const tablesView = async () =>{
         txt = `<h1>Memory_Game</h1>`
         tab.forEach(el => {
             txt += `<table>`
-            for(let j = 0; j < el.length; j++){
-                // console.log([j])
-                if(el[j] === 0){
+            for(let i = 0; i < el.length; i++){
+                // console.log([i])
+                if(el[i] === 0){
                 txt += `<tr><button type="button" class="btn btn-primary m-2" style="width:120px;height:120px" >afficher</button></tr>` 
                 }else{
-                    txt += `<img src='image/elephant.png' style='width:100px; heigtht:100px'>`
+                    txt += `<img src='${getMemory(el[i])}'style='width:100px; heigtht:100px'>`
                 }
                   
             }
-           txt += `</table>`   
+           txt += `</table>` 
         });
+            
+
+        
+              
+        
          result.innerHTML = txt;
 
         
         
-    }catch(err){console.log(console.error(err))}
+    }catch(err){console.error(err)}
 
-    
 }
+const getMemory = (valeur) => {
+    let imgTxt = "image/";
 
+        switch (valeur) {
+            case 1: imgTxt += "elephant.png"
+                break;
+            case 2: imgTxt += "giraffe.png"
+                break;
+            case 3: imgTxt += "hippo.png"
+                break;  
+            case 4: imgTxt += "monkey.png"
+                break;
+            case 5: imgTxt += "panda.png"
+                break;
+            case 6: imgTxt += "parrot.png"
+                break;
+            case 7: imgTxt += "penguin.png"
+                break;
+            case 8: imgTxt += "pig.png"
+                break;
+            case 9: imgTxt += "rabbit.png"
+                break;
+            case 10: imgTxt += "snake.png"
+                break;
+            default: console.log("Cas non pris en compte")
+                break;
+        }
+
+    return imgTxt;
+
+}
 tablesView()
+
+
+
+
+
