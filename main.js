2bccbe9ff1f31aa2305cd1fb33b2ac8bebe19ddf
seeker200025 <barkazaoui04@gmail.com>
2022-02-23 21:49:57 +0100

    add function

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
 const result = document.querySelector('.result');
+const btn = document.querySelector('.btn');
 
-var tab = [
-    [1,0,0,0,],
-    [0,2,0,0,],
-    [0,0,3,0,],
-    [0,4,0,0,]
+let tab = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
+
+let tabResult = [
+    [1,1,2,4],
+    [3,3,4,2],
+    [5,5,6,6],
+    [7,7,8,8],
+    [9,9,10,10],
 ];
 
 
 const tablesView = async () =>{
     
     try{
+        
         let txt = "";
         txt = `<h1>Memory_Game</h1>`
-        tab.forEach(el => {
-            txt += `<table>`
-            for(let i = 0; i < el.length; i++){
-                // console.log([i])
-                if(el[i] === 0){
-                txt += `<tr><button type="button" class="btn btn-primary m-2" style="width:120px;height:120px" >afficher</button></tr>` 
+        for(let i = 0; i < tab.length; i++){
+            txt += `<div>`
+            for(let j = 0; j < tab[i].length; j++){
+                
+                if(tab[i][j] === 0){
+                txt += "<div><button type='button' class='btn btn-primary m-2''style='width:120px;height:120px' onclick ='verif(\""+i+"-"+j+"\")'>afficher</button></div>"
                 }else{
-                    txt += `<img src='${getMemory(el[i])}'style='width:100px; heigtht:100px'>`
+                    txt += `<img src='${getMemory([i][j])}'style='width:100px; heigtht:100px'>`
                 }
                   
             }
-           txt += `</table>` 
-        });
+           txt += `</div>` 
+        };
             
-
-        
-              
-        
          result.innerHTML = txt;
-
-        
         
     }catch(err){console.error(err)}
 
@@ -42,13 +47,13 @@ const getMemory = (valeur) => {
     let imgTxt = "image/";
 
         switch (valeur) {
-            case 1: imgTxt += "elephant.png"
+            case 1: imgTxt += "elephant.jpg";
                 break;
-            case 2: imgTxt += "giraffe.png"
+            case 2: imgTxt += "giraffe.jpg";
                 break;
-            case 3: imgTxt += "hippo.png"
+            case 3: imgTxt += "hippo.jpg";
                 break;  
-            case 4: imgTxt += "monkey.png"
+            case 4: imgTxt += "monkey.png";
                 break;
             case 5: imgTxt += "panda.png"
                 break;
@@ -69,9 +74,23 @@ const getMemory = (valeur) => {
     return imgTxt;
 
 }
+
+
 tablesView()
 
 
+/*
+Programmer la fonction permettant d'afficher l'un des boutons "afficher"
+Faire en sorte de vérifier si les deux images cliquées correspondent
+*/ 
 
+
+const verif = (el) =>{
+    let l = el.substr(0, 1);
+    let c = el.substr(2,1);
+
+    tab[l][c] = tabResult[l][c];
+    tablesView()
+}
 
 
